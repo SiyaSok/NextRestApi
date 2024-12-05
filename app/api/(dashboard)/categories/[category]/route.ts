51c2ac5e2e8ad42a4a6ab3be828d343e73c064f0
siyanda sokhela <SiyandaSokhela@gmail.com>
2024-12-05 16:10:56 +0200

    more fixers

--- app/api/(dashboard)/categories/[category]/route.ts
+++ app/api/(dashboard)/categories/[category]/route.ts
@@ -1,121 +1,58 @@
 /** @format */
 
+import { NextRequest, NextResponse } from "next/server";
 import connect from "@/lib/db";
 import User from "@/lib/modals/users";
 import Category from "@/lib/modals/category";
-import { NextResponse } from "next/server";
 import { Types } from "mongoose";
 
-export const PATCH = async (
-  request: Request,
-  context: { params: { category: string } }
+export const DELETE = async (
+  request: NextRequest,
+  { params }: { params: { category: string } }
 ) => {
-  const categoryId = context.params.category;
+  const categoryId = params.category;
 
   try {
-    const body = await request.json();
-    const { title } = body;
-
-    const { searchParams } = new URL(request.url);
-    const userId = searchParams.get("userId");
+    const userId = request.nextUrl.searchParams.get("userId");
 
     if (!userId || !Types.ObjectId.isValid(userId)) {
-      return new NextResponse("Invalid or missing user ID", { status: 400 });
-    }
-
-    if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
-      return new NextResponse("Invalid or missing category ID", {
-        status: 400,
-      });
-    }
-
-    await connect();
-
-    const user = await User.findById(userId);
-    if (!user) {
-      return new NextResponse(JSON.stringify({ message: "User not found" }), {
-        status: 404,
-      });
-    }
-
-    const category = await Category.findOne({ _id: categoryId, user: userId });
-    if (!category) {
-      return new NextResponse(
-        JSON.stringify({ message: "Category not found" }),
-        { status: 404 }
+      return NextResponse.json(
+        { error: "Invalid or missing user ID" },
+        { status: 400 }
       );
     }
 
-    const updatedCategory = await Category.findByIdAndUpdate(
-      categoryId,
-      { title },
-      { new: true }
-    );
-
-    return new NextResponse(
-      JSON.stringify({
-        message: "Category updated",
-        category: updatedCategory,
-      }),
-      { status: 200 }
-    );
-  } catch (error: unknown) {
-    const errorMessage =
-      error instanceof Error ? error.message : "Unknown error occurred";
-    return new NextResponse("Error in updating the category: " + errorMessage, {
-      status: 500,
-    });
-  }
-};
-
-export const DELETE = async (
-  request: Request,
-  context: { params: { category: string } }
-) => {
-  const categoryId = context.params.category;
-
-  try {
-    const { searchParams } = new URL(request.url);
-    const userId = searchParams.get("userId");
-
-    if (!userId || !Types.ObjectId.isValid(userId)) {
-      return new NextResponse("Invalid or missing user ID", { status: 400 });
-    }
-
     if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
-      return new NextResponse("Invalid or missing category ID", {
-        status: 400,
-      });
+      return NextResponse.json(
+        { error: "Invalid or missing category ID" },
+        { status: 400 }
+      );
     }
 
     await connect();
 
     const user = await User.findById(userId);
     if (!user) {
-      return new NextResponse(JSON.stringify({ message: "User not found" }), {
-        status: 404,
-      });
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
     }
 
     const category = await Category.findOne({ _id: categoryId, user: userId });
     if (!category) {
-      return new NextResponse(
-        JSON.stringify({ message: "Category not found" }),
+      return NextResponse.json(
+        { error: "Category not found" },
         { status: 404 }
       );
     }
 
     await Category.findByIdAndDelete(categoryId);
 
-    return new NextResponse(
-      JSON.stringify({ message: "Category deleted successfully" }),
+    return NextResponse.json(
+      { message: "Category deleted successfully" },
       { status: 200 }
     );
   } catch (error: unknown) {
     const errorMessage =
       error instanceof Error ? error.message : "Unknown error occurred";
-    return new NextResponse("Error in deleting the category: " + errorMessage, {
-      status: 500,
-    });
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
   }
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,7 @@
+/** @format */
 
-'use client'
- 
-import { useState, useEffect } from 'react'
- 
+"use client";
+
 export default function Home() {
-  const [posts, setPosts] = useState(null)
- 
-  useEffect(() => {
-    async function fetchPosts() {
-      const res = await fetch('http://localhost:3000/api/blogs?userId=672af3377664d04aaf053eb1&categoryid=672c4a55fc2840716a533edd&Keywords=Today&enddate=2025-05-17&page=1&limit=10')
-      const data = await res.json()
-      setPosts(data)
-    }
-    fetchPosts()
-  }, [])
- 
-  if (!posts) return <div>Loading...</div>
- 
-  return (
-    <ul>
-      {posts.map((post:[]) => (
-        <li key={post.id}>{post.title}</li>
-      ))}
-    </ul>
-  )
+  return <div></div>;
 }
